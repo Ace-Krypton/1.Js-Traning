af8308d78db7dc80278cc1260cd68e4cc0f217fe
Ace-Krypton <ramizna@code.edu.az>
2021-02-10 23:57:00 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//First
+// console.log(document.getElementById("container"));
+
+// Second
+// console.log(document.querySelector("#container"));
+
+// Third
+// console.log(document.getElementsByClassName("second"));
+// console.log(document.querySelectorAll(".second"));
+
+// Fourth
+// console.log(document.querySelector("ol .third"));
+
+// Fifth
+// let myDiv = document.querySelector("#container");
+// myDiv.innerText = "Hello";
+
+// Sixth
+// let myDiv = document.querySelector(".footer");
+// myDiv.classList.add("main");
+
+// Seventh
+// let myDiv = document.querySelector(".footer");
+// myDiv.classList.remove("main");
+
+// Eighth
+//$0.style.background = 'red';
+
+// Nineth
+// var docTitle = document.title;
+// document.title = newTitle;
+
+// Tenth
+// var myDiv = document.querySelectorAll("ol li");
+// for(var i = 0; i < myDiv.length; i++){
+//     myDiv[i].style.backgroundColor = "green";
+// }
+
+// const title1 = document.queryselector(title);
+// title1.textContent = ("JS");
+// console.log(title1);
